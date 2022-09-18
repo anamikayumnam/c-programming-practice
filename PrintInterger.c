cf7b762df308bf28890fea6f730c010264718726
anamikayumnam <anamika.yumnam1@gmail.com>
2022-09-18 21:30:49 +0530

    Updated print interger

--- PrintInterger.c
+++ PrintInterger.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
+
 int main()
 {
+    printf("Please enter a number.");
 
+    int number;
+
+    scanf("%d", &number);
+
+    printf("You entered %d", number);
+    
     return 0;
 }
